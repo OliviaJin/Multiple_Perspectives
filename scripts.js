b749f2a6e360ba40f6e9f80b39dd5749c7b8d3b0
Yunuuo <jiiny738@newschool.edu>
2023-09-25 18:27:03 -0400

    First commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const section = document.getElementById('perspective1');
+    section.addEventListener('click', function() {
+        alert('You clicked on Perspective 1!');
+    });
+});
